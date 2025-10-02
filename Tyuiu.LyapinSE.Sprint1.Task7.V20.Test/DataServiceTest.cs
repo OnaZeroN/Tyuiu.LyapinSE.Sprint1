24a6fcb209c2b03404ef8fc80a06d3117e237d23
Zeron <dotdot20072607@gmail.com>
2025-10-03 01:39:53 +0500

    feat: Add Task7 test

--- Tyuiu.LyapinSE.Sprint1.Task7.V20.Test/DataServiceTest.cs
+++ Tyuiu.LyapinSE.Sprint1.Task7.V20.Test/DataServiceTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Tyuiu.LyapinSE.Sprint1.Task7.V20.Test
+﻿using Tyuiu.LyapinSE.Sprint1.Task7.V20.Lib;
+
+namespace Tyuiu.LyapinSE.Sprint1.Task7.V20.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -6,7 +8,9 @@
         [TestMethod]
         public void TestCalculate()
         {
-
+            DataService ds = new DataService();
+            double result = ds.Calculate(1, 1);
+            Assert.AreEqual(2.449, result);
         }
     }
 }
